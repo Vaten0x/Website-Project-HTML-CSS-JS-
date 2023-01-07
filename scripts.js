420e3aef7c47d8e628a9aa3e4e67fbf6e4d358da
Jason Lee <113562298+Vaten0x@users.noreply.github.com>
2023-01-07 05:31:43 -0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+alert("Welcome to AP Microeconomic helping website");
+
+n =  new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+d = n.getDate();
+document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
